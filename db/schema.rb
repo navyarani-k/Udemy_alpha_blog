b98d561d42b473eed7788ece92cfd4108d25702e
navyarani-k <navyarani.k@etvbharat.com>
2020-07-04 10:58:10 +0530

    add users table and user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_111901) do
+ActiveRecord::Schema.define(version: 2020_07_04_051140) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
