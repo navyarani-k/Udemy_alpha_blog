419185c0be9d862084aa551917c78c52fd4b6d5e
navyarani-k <navyarani.k@etvbharat.com>
2020-07-13 14:52:36 +0530

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-
+    
+    def show
+      @user = User.find(params[:id])
+      @articles = @user.articles
+    end 
+    
+    def index
+    end
+    
 	def new
 	  @user = User.new
 	end
